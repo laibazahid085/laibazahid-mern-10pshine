48c244bbf1ef764ed04c66ca79292e28d31d9b73
laibazahid085 <laiba140308@gmail.com>
2025-07-17 18:04:01 +0500

    Add User profile

--- Desktop/MERN-10pshine/frontend/src/components/Profile/UserProfile.jsx
+++ Desktop/MERN-10pshine/frontend/src/components/Profile/UserProfile.jsx
@@ -1,49 +1,49 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import "./Profile.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Profile.css";
 
-// const Profile = () => {
-//   const [user, setUser] = useState(null);
+const Profile = () => {
+  const [user, setUser] = useState(null);
 
-//   const getProfile = async () => {
-//     try {
-//       const token = localStorage.getItem("token");
-//       const res = await axios.get("/api/users/profile", {
-//         headers: { Authorization: `Bearer ${token}` },
-//       });
-//       setUser(res.data.user);
-//     } catch (err) {
-//       console.error("Profile fetch failed", err);
-//     }
-//   };
+  const getProfile = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const res = await axios.get("/api/users/profile", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setUser(res.data.user);
+    } catch (err) {
+      console.error("Profile fetch failed", err);
+    }
+  };
 
-//   const handleLogout = () => {
-//     localStorage.removeItem("token");
-//     window.location.href = "/login";
-//   };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  };
 
-//   useEffect(() => {
-//     getProfile();
-//   }, []);
+  useEffect(() => {
+    getProfile();
+  }, []);
 
-//   return (
-//     <div className="profile-container">
-//       <h2>👤 User Profile</h2>
-//       {user ? (
-//         <div className="profile-card">
-//           <p>
-//             <strong>Name:</strong> {user.name}
-//           </p>
-//           <p>
-//             <strong>Email:</strong> {user.email}
-//           </p>
-//           <button onClick={handleLogout}>🚪 Logout</button>
-//         </div>
-//       ) : (
-//         <p>Loading profile...</p>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div className="profile-container">
+      <h2>👤 User Profile</h2>
+      {user ? (
+        <div className="profile-card">
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+          <button onClick={handleLogout}>🚪 Logout</button>
+        </div>
+      ) : (
+        <p>Loading profile...</p>
+      )}
+    </div>
+  );
+};
 
-// export default Profile;
+export default Profile;
